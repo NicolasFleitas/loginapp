cd2aff8df66ec92c00f822f0cbce96d15ded4d2a
oymdev04 <oymdev4@oym.com.py>
2019-11-25 17:03:41 -0300

    Se agrego login y logout

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  public email: string;
+  public password: string;
 
-  constructor() { }
+  constructor(
+    public authservice: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
+
+
+  onSubmitLogin() {
+    this.authservice.loginEmail(this.email, this.password)
+    .then( (res) => {
+      this.router.navigate(['/privado']);
+    }).catch( (err) => {
+      console.log(err);
+      this.router.navigate(['login']);
+    });
+  }
+
 
 }
