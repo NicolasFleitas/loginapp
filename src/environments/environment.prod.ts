7ef0427b63a69341d30c31bcdb7c4ab9b9ed7cec
oymdev04 <oymdev4@oym.com.py>
2019-11-25 14:36:27 -0300

    Configuracion de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {apiKey: "AIzaSyBXhj_6YhpBKobaF_kQccaM9jcyXEOf71g",
+  authDomain: "loginangular-714e4.firebaseapp.com",
+  databaseURL: "https://loginangular-714e4.firebaseio.com",
+  projectId: "loginangular-714e4",
+  storageBucket: "loginangular-714e4.appspot.com",
+  messagingSenderId: "172386786903",
+  appId: "1:172386786903:web:a203767cbbff7d19a1cd12",
+  measurementId: "G-RGPVKJVX9M"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {apiKey: "AIzaSyBXhj_6YhpBKobaF_kQccaM9jcyXEOf71g",
+  authDomain: "loginangular-714e4.firebaseapp.com",
+  databaseURL: "https://loginangular-714e4.firebaseio.com",
+  projectId: "loginangular-714e4",
+  storageBucket: "loginangular-714e4.appspot.com",
+  messagingSenderId: "172386786903",
+  appId: "1:172386786903:web:a203767cbbff7d19a1cd12",
+  measurementId: "G-RGPVKJVX9M"
+}
 };
 
 /*
